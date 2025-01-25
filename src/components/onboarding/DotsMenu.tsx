d779e4ba2a4b438f7023725c1aa5f80123819534
justanexperiment <justanexperimentpark@gmail.com>
2025-01-24 20:15:39 -0800

    Update url

--- src/components/onboarding/DotsMenu.tsx
+++ src/components/onboarding/DotsMenu.tsx
@@ -15,7 +15,7 @@ const DotsMenu = () => {
             className="px-4 py-2 theme-text theme-bg-hover cursor-pointer"
             onClick={() => {
               setShowMenu(false);
-              window.open('https://github.com/athenaleong/obsidian-sr', '_blank'); // TODO @belindamo change the url
+              window.open('https://github.com/ai-learning-tools/obsidian-spaced-repetition-ai', '_blank'); 
             }}
           >
             About Spaced Repetition AI
